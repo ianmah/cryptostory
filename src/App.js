8a3dd8f80bd4fa80f3ae0e74847891994382e66c
danielshmidov <daniel.shmidov@gmail.com>
2021-02-20 15:25:20 -0800

    implement progress bar functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Character from './components/Character'
-import Item from './components/Item'
+import React, { useState } from "react";
+import styled from "styled-components";
+import Character from "./components/Character";
+import Item from "./components/Item";
+import ProgressBar from "./components/ProgressBar";
 
 const Container = styled.div`
   max-width: 600px;
   margin: auto;
   background: #fff;
   height: 100vh;
-`
+`;
 const items = [
   {
-      itemId: 2000,
-      version: "220"
+    itemId: 2000,
+    version: "220",
   },
   {
-      itemId: 12000,
-      version: "220"
-  }
-]
+    itemId: 12000,
+    version: "220",
+  },
+];
 
 function App() {
   const [inventory, setInventory] = useState({
-    items: ['1001021', '1082059']
+    items: ["1001021", "1082059"],
   });
-  
 
   return (
     <Container>
-      <Character character={{items}} action='stand1' />
-      {
-        inventory.items.map(item => <Item id={item} />)
-      }
+      <Character character={{ items }} action="stand1" />
+      {inventory.items.map((item) => (
+        <Item id={item} />
+      ))}
+      <ProgressBar percentage={66} />
     </Container>
   );
 }
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledProgressBar = styled.div`
+  position: relative;
+  height: 20px;
+  width: 350px;
+  border-radius: 50px;
+  border: 1px solid #333;
+`;
+
+const StyledFilter = styled.div`
+  background: #1da598;
+  height: 100%;
+  border-radius: inherit;
+  transition: width 0.2s ease-in;
+`;
+
+const Filter = (props) => {
+  return <StyledFilter style={{ width: `${props.percentage}%` }} />;
+};
+
+const ProgressBar = (props) => {
+  return (
+    <StyledProgressBar>
+      <Filter percentage={props.percentage} />
+    </StyledProgressBar>
+  );
+};
+
+export default ProgressBar;
