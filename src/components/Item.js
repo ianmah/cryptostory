281402f16363fca1682e307e0c3e09f144e47827
unko-chan <kevinliang0303@gmail.com>
2021-02-21 04:25:17 -0800

    add inventory layout styles

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,21 +3,22 @@ import styled from 'styled-components'
 
 // Request URL: https://maplestory.io/api/GMS/220/item/1102144/icon
 const StyledImg = styled.img`
-    min-height: 64px;
-    image-rendering: pixelated;
-    padding: 10px;
-    &:hover {
-        outline: 2px solid green;
-    }
-    &.equipped {
-        box-shadow: inset 0 0 10px green;
-    }
-`
+  height: 64px;
+  image-rendering: pixelated;
+  image-size: 100%;
+  padding: 10px;
+
+`;
+
 const Item = ({id, ...props}) => {
     
-    return (
-        <StyledImg src={`https://maplestory.io/api/GMS/220/item/${id}/icon`} alt='Item' {...props}/>
-    )
+    return ( 
+        <StyledImg
+          src={`https://maplestory.io/api/GMS/220/item/${id}/icon`}
+          alt="Item"
+          {...props}
+        />
+    );
     
 }
 
--- src/components/Storage.js
+++ src/components/Storage.js
@@ -9,26 +9,40 @@ const Container = styled.div`
 `;
 
 const Preview = styled.div`
-  position: relative;
+display: flex;
+justify-content: center;
+align-items: center;
   width: 100%;
   height: 250px;
-  text-align: center;
   background-image: url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d34a59be-b3d6-4b35-a43b-66b0685de866/da1e7rd-72face96-045a-4e6b-9c3f-80437eb11d52.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZDM0YTU5YmUtYjNkNi00YjM1LWE0M2ItNjZiMDY4NWRlODY2XC9kYTFlN3JkLTcyZmFjZTk2LTA0NWEtNGU2Yi05YzNmLTgwNDM3ZWIxMWQ1Mi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.lFhK4oX68ArXWQYRubNQCGxCgt9Cad3991fLSHWyD9Q);
   background-position: left bottom;
   background-size: 250%;
 `;
 
 const AbsoluteCharacter = styled(Character)`
-  margin-left: auto;
-  margin-right: auto;
-  left: 0;
-  right: 0;
-  bottom: 50px;
-  position: absolute;
+
 `;
 
 const InventoryWrapper = styled.div`
-  margin: 1em;
+  display:flex;
+  flex-wrap: wrap;
+`;
+
+const InventoryGrid = styled.div`
+  width: 84px;
+  border: lightgrey solid 1.6px;
+  border-radius: 2px;
+  margin: 0.4em;
+  box-shadow: inset 7px 15px 6px -7px rgba(0, 0, 0, 0.25);
+  transition: all 0.05s ease-in;
+
+  &:hover {
+    box-shadow: inset 7px 15px 20px -7px rgba(0, 0, 0, 0.6);
+  }
+  &.equipped {
+    box-shadow: inset 7px 15px 20px -7px rgba(0, 0, 0, 0.6);
+    border: rgba(0, 0, 0, 0.6) solid 1.6px;
+  }
 `;
 
 const baseBody = {
@@ -79,12 +93,11 @@ const Storage = ({ character = {}, inventory }) => {
       <br />
       <InventoryWrapper>
         {inventory.items.map((item) => (
-          <Item
-            key={item.id}
-            onClick={() => equip(item)}
-            id={item.id}
+          <InventoryGrid
             className={items[item.id] ? 'equipped' : 'not-equipped'}
-          />
+          >
+            <Item key={item.id} onClick={() => equip(item)} id={item.id} />
+          </InventoryGrid>
         ))}
       </InventoryWrapper>
     </Container>
