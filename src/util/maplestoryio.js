8578f9019869de828fa5de33b9f942383a24d53b
unknown <ianmmah@gmail.com>
2021-02-20 17:12:22 -0800

    remove console logs

--- src/util/maplestoryio.js
+++ src/util/maplestoryio.js
@@ -3,12 +3,7 @@ const API = 'https://maplestory.io/api/character'
 const options = 'animated?showears=false&showLefEars=false&showHighLefEars=undefined&resize=2&name=&flipX=false'
 
 export const getCharacter = async (items, action) => {
-
-    console.log(items)
-
     const filteredItems = Object.keys(items).filter(key => items[key])
-
-    console.log(filteredItems)
 
     const formattedItems = filteredItems.map(item => ({
             itemId: item,
